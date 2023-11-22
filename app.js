5a3e0315e657ec907e40bbe02b343315e1cd2a68
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-22 20:31:18 +0100

    plates

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const platesData = require('./data/menu.json');
+const ordersData = require('./data/orders.json');
 
 app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT} 🚀`)
 });
+
+app.get('/plates', (req, res) => {
+  res.json(platesData)
+})
