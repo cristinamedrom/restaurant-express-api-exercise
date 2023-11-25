1eab4d8d0cdae4d4a9e38a74a3f2305e69929b5c
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-25 09:05:45 +0100

    upload

--- app.js
+++ app.js
@@ -12,4 +12,33 @@ app.listen(PORT, () => {
 
 app.get('/plates', (req, res) => {
   res.json(platesData)
-})
+});
+
+app.post('/plates', (req, res) => {
+  const newPlate = req.body;
+
+  res.json({ message: "Plate created", data: newPlate });
+});
+
+app.put('/plates/:id', (req, res) => {
+  const plateById = req.params.id;
+  const updatedData = req.body;
+
+  res.json({ message: "Plate updated", data: updatedData })
+});
+
+app.delete('/plates/:id', (req, res) => {
+  const plateById = req.params.id;
+
+  res.json({ message: "Plate removed", data: { id: plateById } })
+});
+
+app.get('/orders', (req, res) => {
+  res.json(ordersData);
+});
+
+app.post('/orders/create', (req, res) => {
+  const newOrder = req.body;
+
+  res.json({ message: "Order created", data: newOrder });
+});
